f69f436a89bf351693925f1f46e78ba902c359f3
Daniel Fořt <danielfort2005@gmail.com>
2024-08-28 09:00:07 +0000

    feat: add ARG_CONTROL function

--- element.sh
+++ element.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+ARG_CONTROL() {
+  if [[ -z $1 ]]
+  then 
+    echo "Please provide an element as an argument."
+  else 
+
+    echo "finish"
+
+  fi
+}
+
+
+ARG_CONTROL $1
