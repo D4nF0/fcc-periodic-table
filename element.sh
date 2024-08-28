b759cb47ca214d0bfe76fc2d5352fdbe95c059f9
Daniel Fořt <danielfort2005@gmail.com>
2024-08-28 09:21:58 +0000

    feat: add executable permission and type differentiator

--- element.sh
+++ element.sh
@@ -8,9 +8,31 @@ ARG_CONTROL() {
     echo "Please provide an element as an argument."
   else 
 
-    echo "finish"
-
+    if [[ $1 =~ ^[0-9]+$ ]]
+    then
+      SELECTOR $1 atomic_number
+    else 
+      if [[ ${#1} -le 2 ]]
+      then
+        SELECTOR $1 symbol
+      elif [[ ${#1} -ge 4 ]]
+      then
+        SELECTOR $1 name
+      else
+        if [[ $1 == Tin ]]
+        then
+          SELECTOR $1 name
+        else 
+          SELECTOR $1 symbol
+        fi
+      fi
+    fi
+    
   fi
+}
+
+SELECTOR() {
+    echo "$1 $2"
 }
 
 
